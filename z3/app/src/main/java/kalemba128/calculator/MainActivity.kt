3f0ffc7ea87ddc5f3a9c9aed851fdca9c00b6b86
kalemba128 <kalemba128@gmail.com>
2023-11-25 14:02:41 +0100

    z3 - add ui

--- z3/app/src/main/java/kalemba128/calculator/MainActivity.kt
+++ z3/app/src/main/java/kalemba128/calculator/MainActivity.kt
@@ -2,10 +2,18 @@ package kalemba128.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun numberAction(view: View) {}
+
+    fun operatorAction(view: View) {}
+    fun allClearAction(view: View) {}
+    fun backSpaceAction(view: View) {}
+    fun equalsAction(view: View) {}
 }
